df985629e57c2b1cbadc814b600ee8e359a696b8
coleeric <89488377+coleeric@users.noreply.github.com>
2021-09-21 21:56:55 -0400

    Update

---
+++ app/public/js/index.js
@@ -0,0 +1,32 @@
+const Author = {
+    data() {
+      return {
+        "person": {}
+        }
+    },
+    computed:{
+      prettyBirthday(){
+        return dayjs(this.person.dob.date)
+        .format('D MMM YYYY')
+      }
+    },
+    methods:{
+        fetchUserData(){
+            fetch('https://randomuser.me/api/')
+            .then(response => response.json())
+            .then((parsedJson) => {
+                console.log(parsedJson);
+                this.person = parsedJson.results[0]
+            })
+            .catch( err => {
+                console.error(err)
+            })
+        }
+
+    },
+    created() {
+        this.fetchUserData();
+    }
+  }
+  
+Vue.createApp(Author).mount('#authorApp');
